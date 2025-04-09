a84336a1e420214181b307ae9eb42e2de15e6e3a
Ignacio Murube <IgnacioMurubeCrego@users.noreply.github.com>
2025-04-09 09:12:14 +0200

    update Readme

--- practica3FrontEnd/fresh.gen.ts
+++ practica3FrontEnd/fresh.gen.ts
@@ -6,6 +6,7 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $_layout from "./routes/_layout.tsx";
 import * as $index from "./routes/index.tsx";
+import * as $search_index from "./routes/search/index.tsx";
 
 import type { Manifest } from "$fresh/server.ts";
 
@@ -15,6 +16,7 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/_layout.tsx": $_layout,
     "./routes/index.tsx": $index,
+    "./routes/search/index.tsx": $search_index,
   },
   islands: {},
   baseUrl: import.meta.url,
